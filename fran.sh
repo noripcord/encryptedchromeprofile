605d2e81cc25cfbc8fe7233b463ffbd8cdf92d62
noripcord <santaf@gmail.com>
2021-11-17 14:59:48 -0300

    Update fran.sh
    Request the password just once, at init

--- fran.sh
+++ fran.sh
@@ -21,7 +21,9 @@ if [ -d "$THENAME" ]; then
  echo "# $THENAME folder exists, will try to use it" 
 elif [ -f "$THENAME.dmg" ]; then 
    echo "# $THENAME.dmg file found, attach"
-   hdiutil attach -stdinpass "$THENAME.dmg"
+   echo "- Enter password to decrypt $THENAME.dmg: "
+   read -s thepass
+   echo $thepass | hdiutil attach -stdinpass "$THENAME.dmg"
    or_die
    echo "# copy contents into $THENAME"
    cp -r "/Volumes/$THENAME" .
@@ -36,18 +38,21 @@ else
   echo "# No folder Or no DMG, will init"
 fi
 
+if [ "$thepass" == "" ]; then
+  echo "- Enter password to encrypt $THENAME.dmg: "
+  read -s thepass
+fi
+
 echo "# Will open google chrome"
 open_ok
 
 echo "# google chrome did terminate"
 echo "# encrypt folder"
-hdiutil create -encryption -stdinpass -srcfolder "$THENAME" "$THENAME.dmg"
+echo $thepass | hdiutil create -encryption -stdinpass -srcfolder "$THENAME" "$THENAME.dmg"
 or_die
 echo "# delete folder"
 rm -rf "$THENAME"
 or_die
 echo "# success"
 exit 0
-
-
 
