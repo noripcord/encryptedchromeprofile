75d4cc050bd99fc0ea01906cedce3a8529024fb7
noripcord <santaf@gmail.com>
2021-11-17 08:01:31 -0300

    main script

---
+++ fran.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+scriptname=$(basename -- $0)
+THENAME=${scriptname%%.*}
+echo "# using identifier: $THENAME"
+
+open_ok () { 
+ echo "# open chrome"
+ /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir="./$THENAME"
+} 
+
+or_die () {
+ errcode=$?
+ if [ $errcode -ne 0 ]; then 
+  echo "# error, exit $errcode"
+  exit $errcode
+ fi
+}
+
+if [ -d "$THENAME" ]; then 
+ echo "# $THENAME folder exists, will try to use it" 
+elif [ -f "$THENAME.dmg" ]; then 
+   echo "# $THENAME.dmg file found, attach"
+   hdiutil attach -stdinpass "$THENAME.dmg"
+   or_die
+   echo "# copy contents into $THENAME"
+   cp -r "/Volumes/$THENAME" .
+   or_die
+   echo "# detach volume"
+   hdiutil detach "/Volumes/$THENAME"
+   or_die
+   echo "# delete image"
+   rm "$THENAME.dmg"
+   or_die
+else
+  echo "# No folder Or no DMG, will init"
+fi
+
+echo "# Will open google chrome"
+open_ok
+
+echo "# google chrome did terminate"
+echo "# encrypt folder"
+hdiutil create -encryption -stdinpass -srcfolder "$THENAME" "$THENAME.dmg"
+or_die
+echo "# delete folder"
+rm -rf "$THENAME"
+or_die
+echo "# success"
+exit 0
+
+
+
